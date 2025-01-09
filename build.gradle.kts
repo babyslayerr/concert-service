6143855fc9b39806c72f563c6f958d0657b9b366
정종환 <gksqlcxkdns1@naver.com>
2025-01-10 03:34:41 +0900

    🔨 ADD : 관련 라이브러리 및 환경설정파일 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,19 +35,28 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // DB
-	// runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("com.h2database:h2")
+	// swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.projectlombok:lombok")
+
+
+	// DB
+	runtimeOnly("com.mysql:mysql-connector-j")
+	// runtimeOnly("com.h2database:h2")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.mockito:mockito-core")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 	systemProperty("user.timezone", "UTC")
 }
+
