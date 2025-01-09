30f9748d371c0672409836c888d38e66595efacc
정종환 <gksqlcxkdns1@naver.com>
2025-01-10 03:55:08 +0900

    🔨 ADD : 인터셉터 추가를 위한 Config 파일 추가

---
+++ src/main/java/kr/hhplus/be/server/config/spring/WebConfig.java
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.config.spring;
+
+import kr.hhplus.be.server.common.interceptor.QueueInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final QueueInterceptor queueInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 인터셉터 등록 및 경로 설정
+        registry.addInterceptor(queueInterceptor)
+                .addPathPatterns("/**") // 모든 경로에 대해 인터셉터 적용
+                .excludePathPatterns("/token/status", "/token"); // 토큰 발급 및 상태 확인에 대해서는 예외
+    }
+}
