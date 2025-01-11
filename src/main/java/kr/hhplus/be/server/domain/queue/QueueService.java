cde85003a3ca8fa6fef7b75eb11af45d2bf7e944
정종환 <gksqlcxkdns1@naver.com>
2025-01-12 06:14:39 +0900

    🔨 피드백 반영 : 스케줄러에서 비즈니스 로직을 서비스 레이어로 추출

--- src/main/java/kr/hhplus/be/server/domain/queue/QueueService.java
+++ src/main/java/kr/hhplus/be/server/domain/queue/QueueService.java
@@ -3,8 +3,12 @@ package kr.hhplus.be.server.domain.queue;
 import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.presentation.queue.QueueResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -50,5 +54,32 @@ public class QueueService {
     public void removeToken(String tokenUuid) {
         Queue queue = queueRepository.findByUuid(tokenUuid).orElseThrow();
         queueRepository.delete(queue);
+    }
+
+    // 토큰 만료 처리
+    public void expireTokens() {
+        // 활성화 된 것들중에 만료시간이 지난 것
+        List<Queue> expiredTokens = queueRepository.findByIsActiveAndExpireAtBefore("active", LocalDateTime.now());
+        expiredTokens.forEach(queue -> {
+            // 토큰 삭제
+            queueRepository.delete(queue); // 상태 저장
+        });
+    }
+
+    // 토큰 활성화 처리
+    public void activateTokens() {
+        long totalSlot = 100;
+        long activeCount = queueRepository.countByIsActive("active");
+
+        // 활성 토큰이 100개 미만인 경우만 활성화
+        if (activeCount < totalSlot) {
+            long remainingSlots = (totalSlot - activeCount);
+            Pageable pageable = PageRequest.of(0, (int) remainingSlots);
+            List<Queue> tokensForActive = queueRepository.findByIsActiveOrderByCreatedDateAsc("wait",pageable);
+            tokensForActive.forEach(queue -> {
+                queue.activate(); // 상태를 active 로 변경
+                queueRepository.save(queue); // 상태 저장
+            });
+        }
     }
 }
