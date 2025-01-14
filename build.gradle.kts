660cad8088aea6a4542da33ebf2192e5bcb9f4b8
정종환 <gksqlcxkdns1@naver.com>
2025-01-15 07:22:35 +0900

    🔨 HOT FIX swagger-version up : @RestControllerAdvice Error 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	// swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 	implementation("org.projectlombok:lombok")
 
 
