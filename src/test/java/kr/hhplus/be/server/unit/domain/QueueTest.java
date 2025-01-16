784fb6788b6380a464fc952e3c2512ed3677f410
정종환 <gksqlcxkdns1@naver.com>
2025-01-16 16:20:03 +0900

    🔨 ADD 큐 도메인 테스트 추가 : 도메인 테스트 추가

---
+++ src/test/java/kr/hhplus/be/server/unit/domain/QueueTest.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.unit.domain;
+
+
+import kr.hhplus.be.server.domain.queue.Queue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class QueueTest {
+    @Test
+    void 대기열토큰이_활성화_된다(){
+        Queue queue = Queue.builder()
+                .isActive("wait")
+                .createdDate(LocalDateTime.now())
+                .build();
+
+        // when
+        queue.activate();
+
+        // 활성화 상태
+        Assertions.assertEquals("active",queue.getIsActive());
+        // 활성화 만료기간이 생긴다
+        Assertions.assertNotNull(queue.getExpireAt());
+    }
+
+}
