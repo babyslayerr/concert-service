7460b335925f0e83b5f63cbdae71efabf8418f76
정종환 <gksqlcxkdns1@naver.com>
2025-02-07 07:06:02 +0900

    🔨 CHANGE 대기열 토큰 방식 변경 : 은행창구 -> 놀이동산 방식

--- src/main/java/kr/hhplus/be/server/application/queue/QueueScheduler.java
+++ src/main/java/kr/hhplus/be/server/application/queue/QueueScheduler.java
@@ -13,14 +13,29 @@ public class QueueScheduler {
 
     private final QueueService queueService;
 
-    @Scheduled(fixedRate = 5000) // 5초마다 실행
+    // 은행창구 방식 스케줄러
+//    @Scheduled(fixedRate = 5000) // 5초마다 실행
+//    @Transactional
+//    public void expireAndActiveTokens() {
+//        // 1. 토큰 만료 처리
+//        queueService.expireTokens();
+//        // 2. 토큰 활성화 처리
+//        queueService.activateTokens();
+//    }
+    // 놀이공원 방식
+    // 만료와, 활성화는 상관관계가 없으므로 별도 분리
+    @Scheduled(fixedRate = 10000) // 10초마다 실행
     @Transactional
-    public void expireAndActiveTokens() {
-        // 1. 토큰 만료 처리
-        queueService.expireTokens();
-        // 2. 토큰 활성화 처리
-        queueService.activateTokens();
+    public void activeTokens(){
+        // 6000명 활성화
+        long activeCount = 6000;
+        queueService.activateTokens(activeCount);
     }
 
-
+    // 만료처리
+    @Scheduled(fixedRate = 20000) // 20초마다 실행
+    @Transactional
+    public void expireTokens(){
+        queueService.expireTokens();
+    }
 }
--- src/main/java/kr/hhplus/be/server/domain/queue/QueueService.java
+++ src/main/java/kr/hhplus/be/server/domain/queue/QueueService.java
@@ -2,9 +2,11 @@ package kr.hhplus.be.server.domain.queue;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
+import kr.hhplus.be.server.infrastructure.queue.QueueRedisRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class QueueService {
+
 
     private final QueueRepository queueRepository;
 
@@ -40,7 +43,7 @@ public class QueueService {
 
     // 대기순번 계산
     public long getWaitingCount(Queue queue) {
-        return queueRepository.countByCreatedDateBeforeAndIsActive(queue.getCreatedDate(), "wait");
+        return queueRepository.countBeforeWaitingToken(queue.getUuid());
     }
 
     public void removeToken(String tokenUuid) {
@@ -51,29 +54,28 @@ public class QueueService {
     // 토큰 만료 처리
     public void expireTokens() {
         // 활성화 된 것들중에 만료시간이 지난 것
-        List<Queue> expiredTokens = queueRepository.findByIsActiveAndExpireAtBefore("active", LocalDateTime.now());
-        log.info("expire token count: {}",expiredTokens.size());
-        expiredTokens.forEach(queue -> {
-            // 토큰 삭제
-            queueRepository.delete(queue); // 상태 저장
-        });
+        // 30분이 지나면 만료
+        int standardMinute = 30 * 60 * 1000;
+        long deleteCount = queueRepository.deleteExpiredTokens(standardMinute);
+        log.info("expire token count: {}",deleteCount);
     }
 
     // 토큰 활성화 처리
     public void activateTokens() {
         long totalSlot = 100;
-        long activeCount = queueRepository.countByIsActive("active");
+        long activeCount = queueRepository.countByIsActive();
 
         // 활성 토큰이 100개 미만인 경우만 활성화
         if (activeCount < totalSlot) {
             long remainingSlots = (totalSlot - activeCount);
-            Pageable pageable = PageRequest.of(0, (int) remainingSlots);
-            List<Queue> tokensForActive = queueRepository.findByIsActiveOrderByCreatedDateAsc("wait",pageable);
-            log.info("active token count: {}",tokensForActive.size());
-            tokensForActive.forEach(queue -> {
-                queue.activate(); // 상태를 active 로 변경
-                queueRepository.save(queue); // 상태 저장
-            });
+            long activatedCount = queueRepository.addActiveTokens(remainingSlots);
+            log.info("active token count: {}",activatedCount);
         }
+    }
+
+    // 토큰 활성화 처리(오버로딩 놀이공원 방식에 사용)
+    public void activateTokens(long activeCount) {
+        long activatedCount = queueRepository.addActiveTokens(activeCount);
+        log.info("active token count: {}",activatedCount);
     }
 }
