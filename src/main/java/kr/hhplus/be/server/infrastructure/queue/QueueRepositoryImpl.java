2490be183ef8d75167630fd3749a9179a60e742f
정종환 <gksqlcxkdns1@naver.com>
2025-02-07 07:00:10 +0900

    🔨 ADD 대기열 토큰 저장소 구현 변경 : 대기열 토큰 구현 변경 DB -> Redis

--- src/main/java/kr/hhplus/be/server/infrastructure/queue/QueueRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/queue/QueueRepositoryImpl.java
@@ -1,56 +1,56 @@
-package kr.hhplus.be.server.infrastructure.queue;
-
-
-import kr.hhplus.be.server.domain.queue.Queue;
-import kr.hhplus.be.server.domain.queue.QueueRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-// 추후 구현체 변경 DB -> Redis 을 위한 인터페이스,구현 분리
-@Repository
-@RequiredArgsConstructor
-public class QueueRepositoryImpl implements QueueRepository {
-
-    private final QueueJpaRepository queueJpaRepository;
-
-    @Override
-    public Queue save(Queue queue) {
-        return queueJpaRepository.save(queue);
-    }
-
-    @Override
-    public Optional<Queue> findByUuid(String uuid) {
-        return queueJpaRepository.findFirstByUuid(uuid);
-    }
-
-    @Override
-    public long countByCreatedDateBeforeAndIsActive(LocalDateTime createdDate, String isActive) {
-        return queueJpaRepository.countByCreatedDateBeforeAndIsActive(createdDate,isActive);
-    }
-
-    @Override
-    public void delete(Queue queue) {
-        queueJpaRepository.delete(queue);
-    }
-
-    @Override
-    public List<Queue> findByIsActiveAndExpireAtBefore(String active, LocalDateTime now) {
-        return queueJpaRepository.findByIsActiveAndExpireAtBefore(active,now);
-    }
-
-    @Override
-    public long countByIsActive(String active) {
-        return queueJpaRepository.countByIsActive(active);
-    }
-
-    @Override
-    public List<Queue> findByIsActiveOrderByCreatedDateAsc(String isActive, Pageable pageable) {
-        return queueJpaRepository.findByIsActiveOrderByCreatedDateAsc(isActive,pageable);
-    }
-
-}
+//package kr.hhplus.be.server.infrastructure.queue;
+//
+//
+//import kr.hhplus.be.server.domain.queue.Queue;
+//import kr.hhplus.be.server.domain.queue.QueueRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.stereotype.Repository;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.Optional;
+//
+//// 추후 구현체 변경 DB -> Redis 을 위한 인터페이스,구현 분리
+//@Repository
+//@RequiredArgsConstructor
+//public class QueueRepositoryImpl implements QueueRepository {
+//
+//    private final QueueJpaRepository queueJpaRepository;
+//
+//    @Override
+//    public Queue save(Queue queue) {
+//        return queueJpaRepository.save(queue);
+//    }
+//
+//    @Override
+//    public Optional<Queue> findByUuid(String uuid) {
+//        return queueJpaRepository.findFirstByUuid(uuid);
+//    }
+//
+//    @Override
+//    public long countByCreatedDateBeforeAndIsActive(LocalDateTime createdDate, String isActive) {
+//        return queueJpaRepository.countByCreatedDateBeforeAndIsActive(createdDate,isActive);
+//    }
+//
+//    @Override
+//    public void delete(Queue queue) {
+//        queueJpaRepository.delete(queue);
+//    }
+//
+//    @Override
+//    public List<Queue> findByIsActiveAndExpireAtBefore(String active, LocalDateTime now) {
+//        return queueJpaRepository.findByIsActiveAndExpireAtBefore(active,now);
+//    }
+//
+//    @Override
+//    public long countByIsActive(String active) {
+//        return queueJpaRepository.countByIsActive(active);
+//    }
+//
+//    @Override
+//    public List<Queue> findByIsActiveOrderByCreatedDateAsc(String isActive, Pageable pageable) {
+//        return queueJpaRepository.findByIsActiveOrderByCreatedDateAsc(isActive,pageable);
+//    }
+//
+//}
