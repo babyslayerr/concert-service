09e0738f456f48ce84f697a641ea60f583ad7dad
정종환 <gksqlcxkdns1@naver.com>
2025-02-14 04:12:03 +0900

    🔨 ADD EventClass  : 결제완료 이벤트 클래스 추가

---
+++ src/main/java/kr/hhplus/be/server/common/event/CompletedPaymentEvent.java
@@ -0,0 +1,14 @@
+package kr.hhplus.be.server.common.event;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CompletedPaymentEvent {
+    private final Long reservationId;
+    private final Long userId;
+    private final Long seatId;
+    private final Long price;
+}
