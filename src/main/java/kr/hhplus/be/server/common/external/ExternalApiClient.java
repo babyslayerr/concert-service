97e81dc038a24bed4c6e63d18ef96d76000333de
정종환 <gksqlcxkdns1@naver.com>
2025-02-14 04:07:18 +0900

    🔨 ADD 외부 API Client : 외부 API 와 통신할 클라이언트 추가

---
+++ src/main/java/kr/hhplus/be/server/common/external/ExternalApiClient.java
@@ -0,0 +1,16 @@
+package kr.hhplus.be.server.common.external;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalApiClient {
+    public void sendReservationInfo(Long reservationId, Long userId, Long seatId, Long price) {
+        System.out.printf("외부 API 호출 - 예약 ID: %d, 유저 ID: %d, 좌석 ID: %d, 가격: %d%n",
+                reservationId, userId, seatId, price);
+    }
+
+    public void sendPaymentInfo(Long reservationId, Long userId, Long seatId, Long price) {
+        System.out.printf("외부 API 호출 - 예약 ID: %d, 유저 ID: %d, 좌석 ID: %d, 가격: %d%n",
+                reservationId, userId, seatId, price);
+    }
+}
