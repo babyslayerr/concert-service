96d616960d9b7fa2264f497a7bcc563088fb5b26
정종환 <gksqlcxkdns1@naver.com>
2025-02-21 05:27:04 +0900

    🔨 ADD 이벤트 재처리 로직  : outbox 패턴에서의 발행 실패시 재처리 스케줄러 및 로직

--- src/main/java/kr/hhplus/be/server/domain/reservation/ReservationService.java
+++ src/main/java/kr/hhplus/be/server/domain/reservation/ReservationService.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.domain.reservation;
 import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.domain.concert.ConcertSeat;
 import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.infrastructure.reservation.kafka.ReservationKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,9 @@ import java.util.List;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final ReservationCreatedOutboxRepository reservationCreatedOutboxRepository;
+    private final PaymentCreatedOutboxRepository paymentCreatedOutboxRepository;
+    private final ReservationKafkaProducer reservationKafkaProducer;
 
     private static final Logger log = LoggerFactory.getLogger(ReservationService.class);
 
@@ -58,5 +62,33 @@ public class ReservationService {
             reservationRepository.save(reservation);
         });
         return reservationList;
+    }
+
+    public void rePublishReservationCompletion(Long waitMinute) {
+        reservationCreatedOutboxRepository.findAllByStatus(OutboxStatus.PENDING)
+                .stream()
+                .forEach((e)->{
+                    // n 분동안 상태가 Pending 인 경우 메세지 재발행
+                    if(e.getCreatedDateTime().minusMinutes(waitMinute).isAfter(LocalDateTime.now())){
+                        // 메세지 재발행
+                        reservationKafkaProducer.publishCompletedReservation(e.getReservationId());
+                        // 재발송 방지를 위한 기존 메세지 각각 삭제
+                        reservationCreatedOutboxRepository.delete(e);
+                    }
+                });
+    }
+
+    public void rePublishPaymentCompletion(Long waitMinute) {
+        paymentCreatedOutboxRepository.findAllByStatus(OutboxStatus.PENDING)
+                .stream()
+                .forEach((e)->{
+                    // 5분동안 상태가 Pending 인 경우 메세지 재발행
+                    if(e.getCreatedDateTime().minusMinutes(waitMinute).isAfter(LocalDateTime.now())){
+                        // 메세지 재발행
+                        reservationKafkaProducer.publishCompletedReservation(e.getReservationId());
+                        // 재발송 방지를 위한 기존 메세지 각각 삭제
+                        paymentCreatedOutboxRepository.delete(e);
+                    }
+                });
     }
 }
