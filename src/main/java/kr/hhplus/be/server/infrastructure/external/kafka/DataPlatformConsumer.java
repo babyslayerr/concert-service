942c15b25ad88ba345f465b58f430b1c434d67c8
정종환 <gksqlcxkdns1@naver.com>
2025-03-10 17:40:24 +0900

    🔨 MODIFY DataPlatformConsumer : DataPlatform 이 kafka 를 통해 데이터를 직접 수집(더이상 API Call 을 사용하지 않음)

--- src/main/java/kr/hhplus/be/server/infrastructure/external/kafka/DataPlatformConsumer.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/external/kafka/DataPlatformConsumer.java
@@ -1,31 +1,25 @@
 package kr.hhplus.be.server.infrastructure.external.kafka;
 
-import kr.hhplus.be.server.domain.reservation.Reservation;
-import kr.hhplus.be.server.domain.reservation.ReservationRepository;
-import kr.hhplus.be.server.infrastructure.external.ExternalApiClient;
+import kr.hhplus.be.server.domain.reservation.ReservationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class DataPlatformConsumer {
-    private final ReservationRepository reservationRepository;
-    private final ExternalApiClient apiClient;
+public class DataPlatformConsumer { // 외부 데이터 플랫폼 Consumer, 별도 서버 구축 예정
 
     // 예약 완료 토픽에 대해 구독
     @KafkaListener(topics = "completedReservation",groupId = "external")
-    public void sendReservationInfo(String reservationId){
-        Reservation reservation = reservationRepository.findById(Long.valueOf(reservationId)).orElseThrow();
-        // 데이터플랫폼으로 예약정보 전송
-        apiClient.sendReservationInfo(reservation.getId(),reservation.getUser().getId(),reservation.getConcertSeat().getId(),reservation.getPrice());
+    public void sendReservationInfo(ReservationDto reservationDto){
+        System.out.printf("데이터플랫폼의 예약완료 topic 구독 완료 - 예약 ID: %d, 유저 ID: %d, 좌석 ID: %d, 가격: %d%n",
+                reservationDto.getId(), reservationDto.getUserId(), reservationDto.getConcertSeatId() ,reservationDto.getPrice());
     }
 
     // 결제 완료 토픽에 대해 구독
-    @KafkaListener(topics = "completedReservation",groupId = "external")
-    public void sendPaymentInfo(String reservationId){
-        Reservation reservation = reservationRepository.findById(Long.valueOf(reservationId)).orElseThrow();
-        // 데이터플랫폼으로 예약정보 전송
-        apiClient.sendPaymentInfo(reservation.getId(),reservation.getUser().getId(),reservation.getConcertSeat().getId(),reservation.getPrice());
+    @KafkaListener(topics = "completedPayment",groupId = "external")
+    public void sendPaymentInfo(ReservationDto reservationDto){
+        System.out.printf("데이터플랫폼의 결제완료 topic 구독 완료  - 예약 ID: %d, 유저 ID: %d, 좌석 ID: %d, 가격: %d%n",
+                reservationDto.getId(), reservationDto.getUserId(), reservationDto.getConcertSeatId() ,reservationDto.getPrice());
     }
 }
